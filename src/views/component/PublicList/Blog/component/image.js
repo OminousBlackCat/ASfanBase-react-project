5c2fe9c3670a8fe928c5be3098f67e0c4ecf7f39
S-Yuyuko <wz1305290174@gmail.com>
2022-10-24 19:48:10 +0800

    A simple demo of img
    Try to use method 'delete' to update page

--- src/views/component/PublicList/Blog/component/image.js
+++ src/views/component/PublicList/Blog/component/image.js
@@ -5,47 +5,30 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
+import Grid from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const actions = [
+  { icon: <FileCopyIcon />, name: 'Copy' },
+  { icon: <SaveIcon />, name: 'Save' },
+  { icon: <PrintIcon />, name: 'Print' },
+  { icon: <DeleteIcon />, name: 'Delete' },
+];
 
 export default function ListImg() {
-  return (
-    <ImageList sx={{ margin: 'auto', width: '40%' }}>
-      <ImageListItem key="Subheader" cols={2}>
-        <ListSubheader component="div">December</ListSubheader>
-      </ImageListItem>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={item.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${item.title}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
-  );
-}
-
-const itemData = [
+  const [itemData, setItemData] = React.useState([
   {
     img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
     title: 'Breakfast',
     author: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
   },
   {
     img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
@@ -61,21 +44,19 @@ const itemData = [
     img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
     title: 'Coffee',
     author: '@nolanissac',
-    cols: 2,
+
   },
   {
     img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
     title: 'Hats',
     author: '@hjrc33',
-    cols: 2,
+
   },
   {
     img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
     title: 'Honey',
     author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
+
   },
   {
     img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
@@ -91,8 +72,7 @@ const itemData = [
     img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
     title: 'Mushrooms',
     author: '@silverdalex',
-    rows: 2,
-    cols: 2,
+
   },
   {
     img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
@@ -108,6 +88,82 @@ const itemData = [
     img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
     title: 'Bike',
     author: '@southside_customs',
-    cols: 2,
+
   },
-];
+]);
+
+  const func = (e) => {
+    switch(e.name){
+      case 'Delete':
+       if(isVisual === ''){
+        setIsVisual('none')
+      } else {
+        setIsVisual('')
+      }
+    }
+  }
+  
+  const Del = (e) => {
+    const list = itemData
+    itemData.splice(e,1)
+    setItemData([...list])
+  }
+  const [isVisual, setIsVisual] = React.useState('none')
+  return (
+    <Box>
+      <Grid container spacing={2} columns={32}>
+        <Grid xs></Grid>
+        <Grid xs={20}>
+          <ImageList sx={{ margin: 'auto', width: '60%' }}>
+            <ImageListItem key="Subheader" cols={2}>
+              <ListSubheader component="div">December</ListSubheader>
+            </ImageListItem>
+            {itemData.map((item,index) => (
+              <ImageListItem key={item.img}>
+                <Button onClick={(e) => Del(index)} sx={{display: isVisual}} variant="outlined" startIcon={<DeleteIcon />}>
+                  Delete
+                </Button>
+                <img
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.title}
+                  subtitle={item.author}
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                      aria-label={`info about ${item.title}`}
+                    >
+                      <InfoIcon />
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Grid>
+        <Grid xs>
+        <Box sx={{flexGrow: 1}}>
+          <SpeedDial
+            ariaLabel="SpeedDial basic example"
+            sx={{ bottom: '8%', right: '5%', position: 'fixed' }}
+            icon={<SpeedDialIcon />}
+          >
+            {actions.map((action) => (
+              <SpeedDialAction
+                key={action.name}
+                icon={action.icon}
+                tooltipTitle={action.name}
+                onClick={(e) => func(action)}
+              />
+            ))}
+          </SpeedDial>
+        </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
